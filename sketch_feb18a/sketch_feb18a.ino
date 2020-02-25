6acbf0c309df291f846ce6f766926a81ab6469c1
pasgiannis <gp519@ic.ac.uk>
2020-02-25 17:40:45 +0000

    added comments to the code

--- sketch_feb18a/sketch_feb18a.ino
+++ sketch_feb18a/sketch_feb18a.ino
@@ -21,19 +21,27 @@ void setup() {
 }
 //---------------------------------------------------------------//
 void loop() {
-
+//this delay is for waiting 15 seconds until the next measurement
   delay(10000);
+
+  
+  //this checks if the SD card is ready to use
  if (!SD.begin(10)) { //using pin 10 (SS)
+  
+         //if there is a mistake with the code it will print that the initialization failed
           Serial.println("Initialization failed!\n");
           sdInitSuccess = false; //failure
           
         }
+        
+        //if it does not fail it then opens/creates the file and named "GASGASGAS(Giannis Atulit Sydney).txt"
         myFile = SD.open("GASGASGAS(Giannis Atulit Sydney).txt", FILE_WRITE);
+
+        //if the file is successfully created/opened then we do the following
         if (myFile) {
-
-//outputValue = map(sensorValue, 0, 1023, 0, 255);
-           
-
+                     
+          //print success message, print the value we are writing (for fast testing purposes),
+          //write the value, close the file, print done and change line
           Serial.println("File opened successfully.");
           Serial.println("Writing to TEST.text /n  Writing value :");
           Serial.println(analogRead(readLight));
@@ -42,6 +50,7 @@ void loop() {
           Serial.println("Done");
           Serial.println();
         }
+        
         else { //else show error
           Serial.println("Error opeing file.\n");
         }
